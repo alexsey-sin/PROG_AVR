f30b3ed8281ad53e2598c3edb7e601bf93941726
Alexey <alexey-sin@yandex.ru>
2023-10-18 22:57:46 +0300

    in work

--- Boiler_my1/build/Font_Tahoma20.d
+++
@@ -1,4 +0,0 @@
-c:\PROG_AVR_2\Boiler_my1\build\Font_Tahoma20.o:  \
- C:\PROG_AVR_2\_fonts\Font_Tahoma20.c C:\PROG_AVR_2\_fonts\/fonts.h
-
-C:\PROG_AVR_2\_fonts\/fonts.h:
--- Boiler_my2/build/main.d
+++ Boiler_my2/build/main.d
@@ -1,22 +1,22 @@
-c:\PROG_AVR_2\Boiler_my2\build\main.o:  \
- c:\PROG_AVR_2\Boiler_my2\src\main.c c:\PROG_AVR_2\Boiler_my2\src\/main.h \
-  c:\PROG_AVR_2\Boiler_my2\src\/../../_libs_macros/bits_macros.h \
-  c:\PROG_AVR_2\Boiler_my2\src\/../../_lib_tft_ili9486/tft_ili9486.h \
-  c:\PROG_AVR_2\Boiler_my2\src/../../_libs_macros/bits_macros.h \
-  c:\PROG_AVR_2\Boiler_my2\src/../../_fonts/fonts.h \
-  c:\PROG_AVR_2\Boiler_my2\src/tft_ili9486_config.h \
-  c:\PROG_AVR_2\Boiler_my2\src\/../../_fonts/fonts.h
+e:\PROG_AVR\Boiler_my2\build\main.o: e:\PROG_AVR\Boiler_my2\src\main.c \
+  e:\PROG_AVR\Boiler_my2\src\/main.h \
+  e:\PROG_AVR\Boiler_my2\src\/../../_libs_macros/bits_macros.h \
+  e:\PROG_AVR\Boiler_my2\src\/../../_lib_tft_ili9486/tft_ili9486.h \
+  e:\PROG_AVR\Boiler_my2\src/../../_libs_macros/bits_macros.h \
+  e:\PROG_AVR\Boiler_my2\src/../../_fonts/fonts.h \
+  e:\PROG_AVR\Boiler_my2\src/tft_ili9486_config.h \
+  e:\PROG_AVR\Boiler_my2\src\/../../_fonts/fonts.h
 
-c:\PROG_AVR_2\Boiler_my2\src\/main.h:
+e:\PROG_AVR\Boiler_my2\src\/main.h:
 
-c:\PROG_AVR_2\Boiler_my2\src\/../../_libs_macros/bits_macros.h:
+e:\PROG_AVR\Boiler_my2\src\/../../_libs_macros/bits_macros.h:
 
-c:\PROG_AVR_2\Boiler_my2\src\/../../_lib_tft_ili9486/tft_ili9486.h:
+e:\PROG_AVR\Boiler_my2\src\/../../_lib_tft_ili9486/tft_ili9486.h:
 
-c:\PROG_AVR_2\Boiler_my2\src/../../_libs_macros/bits_macros.h:
+e:\PROG_AVR\Boiler_my2\src/../../_libs_macros/bits_macros.h:
 
-c:\PROG_AVR_2\Boiler_my2\src/../../_fonts/fonts.h:
+e:\PROG_AVR\Boiler_my2\src/../../_fonts/fonts.h:
 
-c:\PROG_AVR_2\Boiler_my2\src/tft_ili9486_config.h:
+e:\PROG_AVR\Boiler_my2\src/tft_ili9486_config.h:
 
-c:\PROG_AVR_2\Boiler_my2\src\/../../_fonts/fonts.h:
+e:\PROG_AVR\Boiler_my2\src\/../../_fonts/fonts.h:
